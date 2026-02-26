6be8f396b9157a001bd34f176a61dc4a5e916e6e
Spartak092225 <spartak092225@gmail.com>
2026-02-26 09:59:01 +0100

    Refactor Members component to apply CSS module styles for buttons and list

--- colist-app/src/components/members/Members.jsx
+++ colist-app/src/components/members/Members.jsx
@@ -5,23 +5,23 @@ export default function Members() {
     <>
       <header>
         <div>
-          <button>Back</button>
+          <button className={styles.backBtn}>Back</button>
           <h1>Members</h1>
         </div>
-        <button>
+        <button className={styles.inviteBtn}>
           <img src="" alt="" />
           <p>Invite Member</p>
         </button>
       </header>
       <main>
-        <ul>
+        <ul className={styles.listOfMembers}>
           <li className={styles.memberContainer}>
             <img src="" alt="members avatar" className={styles.memberAvatar} />
-            <div>
+            <div className={styles.memberInfoContainer}>
               <h2>John</h2>
               <p>john@email.com</p>
             </div>
-            <button>Delete</button>
+            <button className={styles.deleteBtn}>Delete</button>
           </li>
         </ul>
       </main>
