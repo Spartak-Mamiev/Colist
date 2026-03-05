f413eb866c59409e0f13b876bb6ecf5335febbd8
spartak092225 <spartak092225@gmail.com>
2026-03-05 10:58:41 +0100

    Implement SignUpPage with form handling and input fields; update styles for better layout and interaction

--- colist-app/src/components/signUpPage/SignUpPage.jsx
+++ colist-app/src/components/signUpPage/SignUpPage.jsx
@@ -4,19 +4,72 @@ import Input from '../ui/input/Input';
 import Button from '../ui/button/Button';
 
 import { FaArrowLeft } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUpPage() {
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    navigate('/');
+  }
+
   return (
     <div className={styles.signUpPage}>
       <header className={styles.pageHeader}>
-        <Link to="/">
+        <Link to="/login">
           <FaArrowLeft />
         </Link>
         <h1>Sign Up</h1>
       </header>
       <main>
-        <Input></Input>
+        <form
+          className={styles.form}
+          onSubmit={handleSubmit}
+        >
+          <Input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Your name"
+            label="Name"
+            labelFor="name"
+            required
+          ></Input>
+          <Input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Your Email"
+            label="Email"
+            labelFor="email"
+            required
+          ></Input>
+          <Input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Your Password"
+            label="Password"
+            labelFor="Password"
+            required
+          ></Input>
+          <Input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Confirm Your Password"
+            required
+          ></Input>
+          <Button
+            type="submit"
+            variant="primary"
+            size="large"
+            fullWidth
+          >
+            Sign Up
+          </Button>
+        </form>
       </main>
     </div>
   );
